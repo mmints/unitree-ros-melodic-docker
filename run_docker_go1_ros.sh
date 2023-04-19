2ea57ee6b6a51afd95ea3be852b48e4ec967506b
Mark Oliver Mints <mmints@uni-koblenz.de>
2023-04-19 11:01:08 +0200

    add variable name to run script

--- run_docker_go1_ros.sh
+++ run_docker_go1_ros.sh
@@ -1,9 +1,16 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    container_name="unitree-ros-melodic"
+else
+    container_name="$1"
+fi
+
 docker run \
     -it --rm \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --mount type=bind,source="$(pwd)/catkin_ws",target=/root/catkin_ws \
     --network=host \
+     --name $container_name \
     uneron/unitree-ros-melodic
-
-#     --name go1-ros-container \
